27232f6ad250a1db3bfcb5c1a7885329f6ab4570
Sandro <28706121+SOsintOps@users.noreply.github.com>
2020-12-07 10:29:33 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 
 #ToDo: Move user.js file to proper directory ~/.mozilla/ http://kb.mozillazine.org/Profile_folder
-#ToDo: VMware tools installation
-#ToDo: set up installation of extra programs: maltego, pycharm, cherrytree, the brothers of sherlock, Holehe
+#ToDo: set up installation of extra programs: maltego, sherlock's brothers,
 #ToDo: set osint report templates for libreoffice
 #ToDo: set choice in the user profile script of your interest
 
@@ -31,27 +30,22 @@ sudo apt install -y wget
 sudo snap install vlc
 sudo -H pip3 install --upgrade youtube_dl
 sudo apt install -y curl
-
-#pls change [PASSWORD] with the current one
-cd ~/Downloads
-
-curl -u osint:[PASSWORD] -O https://inteltechniques.com/osintbooksecure/vm-files.zip
-unzip vm-files.zip -d ~/Desktop/
-
-curl -u osint:[PASSWORD] -O https://inteltechniques.com/osintbooksecure/workflow.zip
-unzip workflow.zip -d ~/Documents/
-
-curl -u osint:[PASSWORD] -O https://inteltechniques.com/osintbooksecure/tools.zip
-unzip tools.zip -d ~/Documents/
-
-curl -u osint:[PASSWORD] -O https://inteltechniques.com/osintbooksecure/templates.zip
-mkdir ~/Documents/templates && unzip templates.zip -d ~/Documents/templates
-
-cd ~/Documents/
-curl -u osint:[PASSWORD] -O https://inteltechniques.com/osintbooksecure/linux.20.txt
-
 cd ~/Downloads/Argos/wallpaper/ 
 sudo cp * ~/Pictures
+
+
+# Bazzell goodies - Please change [PASSWORD] with the current one!
+cd ~/Documents/
+curl -u osint:[PASSWORD] -O https://inteltechniques.com/osintbooksecure/linux.20.txt
+cd ~/Downloads
+curl -u osint:[PASSWORD] -O https://inteltechniques.com/osintbooksecure/vm-files.zip
+unzip vm-files.zip -d ~/Desktop/
+curl -u osint:[PASSWORD] -O https://inteltechniques.com/osintbooksecure/workflow.zip
+unzip workflow.zip -d ~/Documents/
+curl -u osint:[PASSWORD] -O https://inteltechniques.com/osintbooksecure/tools.zip
+unzip tools.zip -d ~/Documents/
+curl -u osint:[PASSWORD] -O https://inteltechniques.com/osintbooksecure/templates.zip
+mkdir ~/Documents/templates && unzip templates.zip -d ~/Documents/templates
 
 
 mkdir ~/Documents/scripts
@@ -62,7 +56,6 @@ cd ~/Desktop/vm-files/icons
 cp * ~/Documents/icons
 cd ~/Desktop/vm-files/shortcuts
 sudo cp * /usr/share/applications/
-#Delete the 'vm-files' file and folder from Desktop if desired
 cd ~/Downloads
 mkdir Programs
 cd Programs
@@ -70,77 +63,69 @@ cp ~/Downloads/Argos/update_osint_tools.sh ~/Downloads/Programs/
 sudo chmod +x update_osint_tools.sh
 sudo -H pip3 install Instalooter
 sudo -H pip3 install Instaloader
+sudo python3 -m pip install pipenv
+sudo apt-get install -y webhttrack
+sudo apt install ffmpeg
+sudo apt-get install -y mediainfo-gui
+sudo apt install -y libimage-exiftool-perl
+
+# Amass
+sudo snap install amass
 
 # Twint
+cd ~/Downloads/Programs
 git clone https://github.com/twintproject/twint.git
 cd twint
 sudo -H pip3 install twint
 sudo -H pip3 install -r requirements.txt
-cd ~/Downloads/Programs
 
 # EyeWitness
+cd ~/Downloads/Programs
 git clone https://github.com/ChrisTruncer/EyeWitness.git
 cd EyeWitness/Python/setup
 sudo -H ./setup.sh
 cd ~/Documents/scripts
 sed -i "s/Programs\/EyeWitness/Programs\/EyeWitness\/Python/g" eyewitness.sh
-cd ~/Downloads/Programs
-
-# Amass
-sudo snap install amass
 
 # sublist3r
+cd ~/Downloads/Programs
 git clone https://github.com/aboul3la/Sublist3r.git
 cd Sublist3r && sudo -H pip3 install -r requirements.txt
-cd ~/Downloads/Programs
 
 #Photon
+cd ~/Downloads/Programs
 git clone https://github.com/s0md3v/Photon.git
 cd Photon && sudo -H pip3 install -r requirements.txt
-cd ~/Downloads/Programs
 
 # The Harvester
+cd ~/Downloads/Programs
 git clone https://github.com/laramies/theHarvester.git
 cd theHarvester
 git checkout 8b88a66
 sudo -H pip3 install -r requirements.txt
-cd ~/Downloads/Programs
-
-# Tools
-sudo python3 -m pip install pipenv
-sudo apt-get install -y webhttrack
-
-
-# Modify scripts
-cd ~/Documents/scripts/
-sed -i "s/python3\.6/python3/g" domains.sh
-sed -i "s/python\ sublist3r/python3\ sublist3r/g" domains.sh
-sed -i "s/pip\ install/pip3\ install/g" updates.sh
-sed -i "s/sudo\ pip3/sudo\ \-H\ pip3/g" updates.sh
-pip3 install --upgrade pip
-cd ~/Downloads/Programs
 
 # Metagoofil
+cd ~/Downloads/Programs
 git clone https://github.com/opsdisk/metagoofil.git
 cd metagoofil
 sudo -H pip3 install -r requirements.txt
-cd ~/Downloads/Programs
 
 # recon-ng
+cd ~/Downloads/Programs
 git clone https://github.com/lanmaster53/recon-ng.git
 cd recon-ng
 sudo -H pip3 install -r REQUIREMENTS
-cd ~/Downloads/Programs
 
 # sherlock
+cd ~/Downloads/Programs
 git clone https://github.com/sherlock-project/sherlock.git
 cd sherlock
 python3 -m pip install -r requirements.txt
 cd ~/Documents/scripts/
 sed -i 's/Programs\/sherlock/Programs\/sherlock\/sherlock/g' sherlock.sh
-cd ~/Downloads/Programs
 
 # spiderfoot
+cd ~/Downloads/Programs
 git clone https://github.com/smicallef/spiderfoot.git
 cd spiderfoot
 sudo -H pip3 install -r requirements.txt
@@ -151,9 +136,9 @@ echo "cd ~/Downloads/Programs/spiderfoot" >> spiderfoot.sh
 echo "python3 sf.py -l 127.0.0.1:5001 &" >> spiderfoot.sh
 echo "sleep 5" >> spiderfoot.sh
 echo "firefox http://127.0.0.1:5001" >> spiderfoot.sh
-cd ~/Downloads/Programs
 
 # Elasticsearch-Crawler
+cd ~/Downloads/Programs
 git clone https://github.com/AmIJesse/Elasticsearch-Crawler.git
 cd Elasticsearch-Crawler
 sudo -H pip3 install nested-lookup
@@ -164,33 +149,49 @@ cd ~/Downloads/
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb
 sudo dpkg -i ripgrep_11.0.2_amd64.deb
 
-#FFMpeg
-sudo apt install ffmpeg
+#holehe
+cd ~/Downloads/Programs
+git clone https://github.com/megadose/holehe.git
+cd holehe
+python3 setup.py install
 
-#Mediainfo
-sudo apt-get install -y mediainfo-gui
+#Moriarty-Project
+#cd ~/Downloads/Programs
+#git clone https://github.com/AzizKpln/Moriarty-Project
+#cd Moriarty-Project
+#chmod 755 install.sh ./install.sh
 
-#Exiftool
-sudo apt install -y libimage-exiftool-perl
+#maigret
+#cd ~/Downloads/Programs
+#git clone https://github.com/soxoj/maigret
+#cd maigret
+#pip3 install .
 
 
-#google earth
+#Google earth (still unable to fix an issue with the package key)
 #sudo wget -O google-earth64.deb http://dl.google.com/dl/earth/client/current/google-earth-stable_current_amd64.deb
 #sudo dpkg -i google-earth64.deb
 #sudo apt-get -f install; sudo rm google-earth64.deb
 
-#amass
-sudo snap install amass
+# Modify scripts
+cd ~/Documents/scripts/
+sed -i "s/python3\.6/python3/g" domains.sh
+sed -i "s/python\ sublist3r/python3\ sublist3r/g" domains.sh
+sed -i "s/pip\ install/pip3\ install/g" updates.sh
+sed -i "s/sudo\ pip3/sudo\ \-H\ pip3/g" updates.sh
+pip3 install --upgrade pip
+cd ~/Downloads/Programs
 
 # General Purpose tools
-sudo apt install ubuntu-restricted-extras p7zip unrar
-sudo apt install fonts-crosextra-caladea fonts-crosextra-carlito
-sudo apt install openjdk-11-jre
 sudo snap install cherrytree
-#sudo apt install calibre
 sudo apt install audacity
 sudo snap install atom --classic
 sudo snap install keepassxc
 sudo apt install -y kazam
+sudo apt install openjdk-11-jre
+sudo apt install ubuntu-restricted-extras p7zip unrar
+sudo apt install fonts-crosextra-caladea fonts-crosextra-carlito
+
+#sudo apt install calibre
 #sudo snap install pycharm-professional --classic --edge
-sudo snap install telegram-desktop
+#sudo snap install telegram-desktop

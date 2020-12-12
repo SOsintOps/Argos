b0c4c40b430d53e4ee61408cd042423ddd0fb604
Sandro <28706121+SOsintOps@users.noreply.github.com>
2020-12-12 01:14:39 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -38,7 +38,6 @@ cd ~/Downloads  && curl -u osint:PASSWORD -O https://inteltechniques.com/osintbo
 # the date_osint_tools.sh is copied in the program folder
 mkdir -p ~/Downloads/Programs && cp ~/Downloads/Argos/update_osint_tools.sh ~/Downloads/Programs/
 
-# Requirements for installing tools, scripts and launchers
 echo '#######################################################################'
 echo '#      Requirements for installing tools, scripts and launchers       #'
 echo '#######################################################################'
@@ -60,24 +59,20 @@ sudo -H pip3 install Instalooter
 sudo -H pip3 install Instaloader
 sudo python3 -m pip install pipenv
 
-
 echo '#######################################################################'
 echo '#                             Amass                                   #'
 echo '#######################################################################'
 sudo snap install amass
 
-# Twint
 echo '#######################################################################'
 echo '#                             Twint                                   #'
 echo '#######################################################################'
-
 cd ~/Downloads/Programs
 git clone https://github.com/twintproject/twint.git
 cd twint
 sudo -H pip3 install twint
 sudo -H pip3 install -r requirements.txt
 
-# EyeWitness
 echo '#######################################################################'
 echo '#                          EyeWitness                                 #'
 echo '#######################################################################'
@@ -88,7 +83,6 @@ sudo -H ./setup.sh
 cd ~/Documents/scripts
 sed -i "s/Programs\/EyeWitness/Programs\/EyeWitness\/Python/g" eyewitness.sh
 
-# sublist3r
 echo '#######################################################################'
 echo '#                           sublist3r                                 #'
 echo '#######################################################################'
@@ -96,7 +90,6 @@ cd ~/Downloads/Programs
 git clone https://github.com/aboul3la/Sublist3r.git
 cd Sublist3r && sudo -H pip3 install -r requirements.txt
 
-# sublist3r
 echo '#######################################################################'
 echo '#                              Photon                                 #'
 echo '#######################################################################'
@@ -104,7 +97,6 @@ cd ~/Downloads/Programs
 git clone https://github.com/s0md3v/Photon.git
 cd Photon && sudo -H pip3 install -r requirements.txt
 
-# The Harvester
 echo '#######################################################################'
 echo '#                       The Harvester                                 #'
 echo '#######################################################################'
@@ -114,7 +106,6 @@ cd theHarvester
 git checkout 8b88a66
 sudo -H pip3 install -r requirements.txt
 
-# Metagoofil
 echo '#######################################################################'
 echo '#                           Metagoofil                                #'
 echo '#######################################################################'
@@ -123,7 +114,6 @@ git clone https://github.com/opsdisk/metagoofil.git
 cd metagoofil
 sudo -H pip3 install -r requirements.txt
 
-# recon-ng
 echo '#######################################################################'
 echo '#                              recon-ng                               #'
 echo '#######################################################################'
@@ -132,7 +122,6 @@ git clone https://github.com/lanmaster53/recon-ng.git
 cd recon-ng
 sudo -H pip3 install -r REQUIREMENTS
 
-# sherlock
 echo '#######################################################################'
 echo '#                              sherlock                               #'
 echo '#######################################################################'
@@ -143,7 +132,6 @@ python3 -m pip install -r requirements.txt
 cd ~/Documents/scripts/
 sed -i 's/Programs\/sherlock/Programs\/sherlock\/sherlock/g' sherlock.sh
 
-# spiderfoot
 echo '#######################################################################'
 echo '#                            spiderfoot                               #'
 echo '#######################################################################'
@@ -159,7 +147,6 @@ echo "python3 sf.py -l 127.0.0.1:5001 &" >> spiderfoot.sh
 echo "sleep 5" >> spiderfoot.sh
 echo "firefox http://127.0.0.1:5001" >> spiderfoot.sh
 
-# Elasticsearch-Crawler
 echo '#######################################################################'
 echo '#                     Elasticsearch-Crawler                           #'
 echo '#######################################################################'
@@ -169,7 +156,6 @@ cd Elasticsearch-Crawler
 sudo -H pip3 install nested-lookup
 sudo -H pip3 install internetarchive
 
-#Ripgrep
 echo '#######################################################################'
 echo '#                               Ripgrep                               #'
 echo '#######################################################################'
@@ -177,7 +163,6 @@ cd ~/Downloads/
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb
 sudo dpkg -i ripgrep_11.0.2_amd64.deb
 
-#holehe
 echo '#######################################################################'
 echo '#                                holehe                               #'
 echo '#######################################################################'
@@ -186,7 +171,6 @@ git clone https://github.com/megadose/holehe.git
 cd holehe
 sudo python3 setup.py install
 
-#anonsurf (credit to Und3rf10w)
 echo '#######################################################################'
 echo '#                              anonsurf                               #'
 echo '#######################################################################'
@@ -195,7 +179,6 @@ git clone https://github.com/Und3rf10w/kali-anonsurf.git
 cd kali-anonsurf/
 sudo ./installer.sh
 
-#Moriarty-Project
 echo '#######################################################################'
 echo '#                        Moriarty-Project                             #'
 echo '#######################################################################'
@@ -204,7 +187,6 @@ git clone https://github.com/AzizKpln/Moriarty-Project
 cd Moriarty-Project
 chmod 755 install.sh ./install.sh
 
-#maigret
 echo '#######################################################################'
 echo '#                               maigret                               #'
 echo '#######################################################################'
@@ -216,7 +198,6 @@ pip3 install .
 echo '#######################################################################'
 echo '#                           Google Earth                              #'
 echo '#######################################################################'
-
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo wget -O google-earth64.deb http://dl.google.com/dl/earth/client/current/google-earth-stable_current_amd64.deb
 sudo dpkg -i google-earth64.deb && sudo apt -f install && sudo rm google-earth64.deb
@@ -224,11 +205,9 @@ sudo dpkg -i google-earth64.deb && sudo apt -f install && sudo rm google-earth64
 echo '#######################################################################'
 echo '#                               Maltego                               #'
 echo '#######################################################################'
-
 sudo wget -O maltego.deb https://maltego-downloads.s3.us-east-2.amazonaws.com/linux/Maltego.v4.2.14.13579.deb
 sudo dpkg -i maltego.deb && sudo apt -f install && sudo rm maltego.deb
 
-# Modify scripts
 echo '#######################################################################'
 echo '#                       Customising scripts                           #'
 echo '#######################################################################'
@@ -240,7 +219,6 @@ sed -i "s/sudo\ pip3/sudo\ \-H\ pip3/g" updates.sh
 pip3 install --upgrade pip
 cd ~/Downloads/Programs
 
-# General Purpose tools
 echo '#######################################################################'
 echo '#                       General Purpose tools                         #'
 echo '#######################################################################'
@@ -255,56 +233,5 @@ sudo apt install -y ripgrep
 sudo apt install -y ubuntu-restricted-extras p7zip unrar
 sudo apt install -y fonts-crosextra-caladea fonts-crosextra-carlito
 cd ~/Documents && git clone https://github.com/pstirparo/threatintel-resources
-
-#echo '#######################################################################'
-#echo '#                         WORK IN PROGRESS                            #'
-#echo '#######################################################################'
-#https://github.com/ivolo/disposable-email-domains
-#https://github.com/jsvine/waybackpack
-#https://github.com/UnkL4b/GitMiner
-#https://github.com/jarun/googler
-#https://github.com/bjoernkarmann/project_alias
-#https://github.com/sundowndev/PhoneInfoga
-#https://github.com/graniet/operative-framework
-#https://github.com/firstlookmedia/dangerzone
-#https://github.com/lulz3xploit/LittleBrother
-#https://github.com/Lazza/Carbon14
-#https://github.com/woj-ciech/Kamerka-GUI
-
-#echo '#######################################################################'
-#echo '#                               Hunchly                               #'
-#echo '#######################################################################'
-#https://support.hunch.ly/article/15-installing-hunchly-on-linux
-
-#at first ... chrome! mmm what about Chromium ?
-#cd ~/Downloads/Programs
-#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-#sudo apt install ./google-chrome-stable_current_amd64.deb
-
-#WHAT ABOUT CHROMIUM? Brave?
-#sudo apt install chromium-browser
-
-#wget https://www.hunch.ly/installers/hunchly.deb
-#sudo apt install ./hunchly.deb
-#sudo apt --fix-broken install
-
-#echo '#######################################################################'
-#echo '#                             userrecon                               #'
-#echo '#######################################################################'
-#https://github.com/stjordanis/userrecon-py
-#sudo -H pip3 install git+https://github.com/stjordanis/userrecon-py.git
-#userrecon-py --help
-
-#echo '#######################################################################'
-#echo '#                               fawkes                                #'
-#echo '#######################################################################'
-#https://github.com/Shawn-Shan/fawkes
-
-
-
-
-#sudo apt install calibre
-#sudo snap install pycharm-professional --classic --edge
-#sudo snap install telegram-desktop
 
 sudo reboot now

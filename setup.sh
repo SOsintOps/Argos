fba57d720ba3a61b2a2602fb580550d530158e7e
Sandro <28706121+SOsintOps@users.noreply.github.com>
2020-12-12 01:16:13 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -34,8 +34,7 @@ cd ~/Downloads  && curl -u osint:PASSWORD -O https://inteltechniques.com/osintbo
 
 cd ~/Downloads  && curl -u osint:PASSWORD -O https://inteltechniques.com/osintbooksecure/templates.zip && mkdir ~/Documents/templates && unzip templates.zip -d ~/Documents/templates
 
-
-# the date_osint_tools.sh is copied in the program folder
+# the update_osint_tools.sh is copied in the Programs folder
 mkdir -p ~/Downloads/Programs && cp ~/Downloads/Argos/update_osint_tools.sh ~/Downloads/Programs/
 
 echo '#######################################################################'

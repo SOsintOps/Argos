e0934ec2518288ff9cb153f2020073affac187c3
stanleyhal <37350050+stanleyhal@users.noreply.github.com>
2020-12-12 19:16:10 +0100

    Add files via upload

---
+++ scripts/background.sh
@@ -0,0 +1,19 @@
+case $DESKTOP_SESSION in
+"xfce")
+	xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVirtual1/workspace0/last-image -s "/home/osint/Pictures/Be-quiet-Priest-sculpture-in-Venlo.jpg" 
+	;;
+"ubuntu-wayland")
+	gsettings set org.gnome.desktop.background picture-uri 'file:///home/osint/Pictures/Be-quiet-Priest-sculpture-in-Venlo.jpg'
+	;;
+"gnome" | "ubuntu")
+	gsettings set org.gnome.desktop.background picture-uri 'file:///home/osint/Pictures/Be-quiet-Priest-sculpture-in-Venlo.jpg'
+	;;
+"i3")
+	feh --bg-scale ~/Pictures/image.jpg # set
+	echo "exec feh --bg-scale /home/osint/Pictures/Be-quiet-Priest-sculpture-in-Venlo.jpg" >> ~/.config/i3/config 
+	;;
+*)
+echo Unknown graphical system.
+;;
+esac
+

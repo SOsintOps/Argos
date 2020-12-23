01b63d8235410e669cf0daefea3e6c563c62d9e6
Sandro <28706121+SOsintOps@users.noreply.github.com>
2020-12-23 18:48:23 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -37,7 +37,8 @@ sudo apt -y update && sudo apt -y upgrade
 # sudo add-apt-repository multiverse
 # sudo apt install virtualbox-guest-dkms virtualbox-guest-x11
 # sudo adduser osint vboxsf
-
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
 echo '#######################################################################'
 echo '#                            A few scripts                           #'
 echo '#######################################################################'
@@ -69,7 +70,8 @@ mkdir -p ~/Downloads/Programs && cp ~/Downloads/Argos/update_osint_tools.sh ~/Do
 
 # setting the 'quiet priest' background image
 cp ~/Downloads/Argos/multimedia/wallpapers/* ~/Pictures && ~/Documents/scripts/background.sh
-
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
 
 echo '#######################################################################'
 echo '#      Requirements for installing tools, scripts and launchers       #'
@@ -91,11 +93,15 @@ sudo -H pip3 install --upgrade youtube_dl
 sudo -H pip3 install Instalooter
 sudo -H pip3 install Instaloader
 sudo python3 -m pip install pipenv
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
 
 echo '#######################################################################'
 echo '#                             Amass                                   #'
 echo '#######################################################################'
 sudo snap install amass
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
 
 echo '#######################################################################'
 echo '#                             Twint                                   #'
@@ -104,6 +110,8 @@ echo '#######################################################################'
 git clone https://github.com/twintproject/twint.git ~/Downloads/Programs/twint
 sudo -H pip3 install ~/Downloads/Programs/twint
 sudo -H pip3 install -r ~/Downloads/Programs/twint/requirements.txt
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
 
 echo '#######################################################################'
 echo '#                          EyeWitness                                 #'
@@ -113,6 +121,8 @@ git clone https://github.com/ChrisTruncer/EyeWitness.git ~/Downloads/Programs/Ey
 sudo -H ~/Downloads/Programs/EyeWitness/setup.sh
 cd ~/Documents/scripts
 sed -i "s/Programs\/EyeWitness/Programs\/EyeWitness\/Python/g" eyewitness.sh
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
 
 echo '#######################################################################'
 echo '#                           sublist3r                                 #'
@@ -120,6 +130,8 @@ echo '#######################################################################'
 
 git clone https://github.com/aboul3la/Sublist3r.git ~/Downloads/Programs/Sublist3r
 sudo -H pip3 install -r ~/Downloads/Programs/Sublist3r/requirements.txt
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
 
 echo '#######################################################################'
 echo '#                              Photon                                 #'
@@ -127,6 +139,8 @@ echo '#######################################################################'
 
 git clone https://github.com/s0md3v/Photon.git ~/Downloads/Programs/Photon
 sudo -H pip3 install -r ~/Downloads/Programs/Photon/requirements.txt
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
 
 echo '#######################################################################'
 echo '#                       The Harvester                                 #'
@@ -136,6 +150,8 @@ git clone https://github.com/laramies/theHarvester.git ~/Downloads/Programs/theH
 cd ~/Downloads/Programs/theHarvester
 git checkout 8b88a66
 sudo -H pip3 install -r requirements.txt
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
 
 echo '#######################################################################'
 echo '#                           Metagoofil                                #'
@@ -143,6 +159,8 @@ echo '#######################################################################'
 
 git clone https://github.com/opsdisk/metagoofil.git ~/Downloads/Programs/metagoofil
 sudo -H pip3 install -r ~/Downloads/Programs/metagoofil/requirements.txt
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
 
 echo '#######################################################################'
 echo '#                              recon-ng                               #'
@@ -151,6 +169,8 @@ echo '#######################################################################'
 git clone https://github.com/lanmaster53/recon-ng.git ~/Downloads/Programs/recon-ng
 cd ~/Downloads/Programs/recon-ng
 sudo -H pip3 install -r REQUIREMENTS
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
 
 echo '#######################################################################'
 echo '#                              sherlock                               #'
@@ -160,6 +180,8 @@ git clone https://github.com/sherlock-project/sherlock.git ~/Downloads/Programs/
 python3 -m pip install -r ~/Downloads/Programs/sherlock/requirements.txt
 cd ~/Documents/scripts/
 sed -i 's/Programs\/sherlock/Programs\/sherlock\/sherlock/g' sherlock.sh
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
 
 echo '#######################################################################'
 echo '#                            spiderfoot                               #'
@@ -174,6 +196,8 @@ echo "cd ~/Downloads/Programs/spiderfoot" >> spiderfoot.sh
 echo "python3 sf.py -l 127.0.0.1:5001 &" >> spiderfoot.sh
 echo "sleep 5" >> spiderfoot.sh
 echo "firefox http://127.0.0.1:5001" >> spiderfoot.sh
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
 
 echo '#######################################################################'
 echo '#                     Elasticsearch-Crawler                           #'
@@ -183,6 +207,9 @@ git clone https://github.com/AmIJesse/Elasticsearch-Crawler.git ~/Downloads/Prog
 cd ~/Downloads/Programs/Elasticsearch-Crawler
 sudo -H pip3 install nested-lookup
 sudo -H pip3 install internetarchive
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
+
 
 echo '#######################################################################'
 echo '#                               Ripgrep                               #'
@@ -190,6 +217,8 @@ echo '#######################################################################'
 cd ~/Downloads/
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb
 sudo dpkg -i ripgrep_11.0.2_amd64.deb
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
 
 echo '#######################################################################'
 echo '#                                holehe                               #'
@@ -197,6 +226,8 @@ echo '#######################################################################'
 
 git clone https://github.com/megadose/holehe.git ~/Downloads/Programs/holehe
 cd ~/Downloads/Programs/holehe && sudo python3 setup.py install
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
 
 echo '#######################################################################'
 echo '#                              anonsurf                               #'
@@ -204,6 +235,8 @@ echo '#######################################################################'
 
 git clone https://github.com/Und3rf10w/kali-anonsurf.git ~/Downloads/Programs/kali-anonsurf
 cd ~/Downloads/Programs/kali-anonsurf && sudo ./installer.sh
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
 
 echo '#######################################################################'
 echo '#                        Moriarty-Project                             #'
@@ -211,6 +244,8 @@ echo '#######################################################################'
 
 git clone https://github.com/AzizKpln/Moriarty-Project ~/Downloads/Programs/Moriarty-Project
 cd ~/Downloads/Programs/Moriarty-Project && chmod 755 install.sh ./install.sh
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
 
 echo '#######################################################################'
 echo '#                               maigret                               #'
@@ -218,6 +253,9 @@ echo '#######################################################################'
 
 git clone https://github.com/soxoj/maigret ~/Downloads/Programs/maigret
 pip3 install ~/Downloads/Programs/maigret/.
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
+
 
 echo '#######################################################################'
 echo '#                           Google Earth                              #'
@@ -225,12 +263,18 @@ echo '#######################################################################'
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo wget -O google-earth64.deb http://dl.google.com/dl/earth/client/current/google-earth-stable_current_amd64.deb
 sudo dpkg -i google-earth64.deb && sudo apt -f install && sudo rm google-earth64.deb
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
+
 
 echo '#######################################################################'
 echo '#                               Maltego                               #'
 echo '#######################################################################'
 wget -O maltego.deb https://maltego-downloads.s3.us-east-2.amazonaws.com/linux/Maltego.v4.2.14.13579.deb
 sudo dpkg -i maltego.deb && sudo apt -f install && rm maltego.deb
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
+
 
 echo '#######################################################################'
 echo '#                       Customising scripts                           #'
@@ -242,6 +286,8 @@ sed -i "s/pip\ install/pip3\ install/g" updates.sh
 sed -i "s/sudo\ pip3/sudo\ \-H\ pip3/g" updates.sh
 sudo pip3 install --upgrade pip
 cd ~/Downloads/Programs
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
 
 echo '#######################################################################'
 echo '#                       General Purpose tools                         #'
@@ -259,3 +305,6 @@ sudo apt install -y openshot
 #sudo apt install -y ubuntu-restricted-extras
 git clone https://github.com/pstirparo/threatintel-resources ~/Documents/threatintel-resources
 sudo apt autoremove -y
+echo -e "$OKRED +----=[Si vis pacem, para bellum]=----+ $RESET"
+echo "    "
+

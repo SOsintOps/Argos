69e42f55fbe5cbd6aa756e87d03722330b8c7beb
stanleyhal <37350050+stanleyhal@users.noreply.github.com>
2022-06-11 20:00:49 +0200

    Update spiderfoot.sh

--- scripts/spiderfoot.sh
+++ scripts/spiderfoot.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/smicallef/spiderfoot.git
 cd spiderfoot
-~/spiderfoot$ pip install -r requirements.txt
+pip install -r requirements.txt
 firefox http://localhost:5009
 #start with command python3 ./sf.py -l 127.0.0.1:5001
 #browse to http://127.0.0.1:5001/
